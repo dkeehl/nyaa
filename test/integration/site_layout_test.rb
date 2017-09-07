c43df55746cce72a2e675e0daf6b96590ff9c32d
Thomas Lin <dkeehl@gmail.com>
2017-09-07 11:04:00 +0800

    add search filters to navbar

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test 'should get root' do
     get root_url
     assert_template 'torrents/index'
-    #assert_select 'div.pagination'
+    assert_select 'div.pagination'
   end
 
 end
